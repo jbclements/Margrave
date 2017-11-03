b6d3039c5d741f18d345b4b047a48e0bccdb9e74
John Clements <clements@racket-lang.org>
2017-11-03 09:03:43 -0700

    gitignore, info.rkt

--- info.rkt
+++ info.rkt
@@ -1,6 +1,8 @@
-#lang setup/infotab
+#lang info
+
 (define name "Margrave")
 (define scribblings '(["scribblings/margrave.scrbl" ]))
-
+(define collection "margrave")
+(define compile-omit-paths '("extra-files" "extended-examples"))
 ;(multi-page)  ; multi-page would cause each top-level sect to get its own html page
 ; see raco setup docs
