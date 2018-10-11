09780169700c463def0d6c66192f3b07048671d8
John Clements <clements@brinckerhoff.org>
2018-10-11 11:17:20 -0700

    add dependencies to info.rkt

--- info.rkt
+++ info.rkt
@@ -6,3 +6,14 @@
 (define compile-omit-paths '("extra-files" "extended-examples"))
 ;(multi-page)  ; multi-page would cause each top-level sect to get its own html page
 ; see raco setup docs
+
+(define deps
+  (list "base"
+        "gui-lib"
+        "parser-tools-lib"
+        "rackunit-lib"
+        "scheme-lib"
+        "srfi-lite-lib"))
+(define build-deps
+  (list "racket-doc"
+        "scribble-lib"))
